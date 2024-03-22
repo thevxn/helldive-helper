6a45c2513b0d6d95ce608de1029b00b2d3fa5de8
Tackx <vlk.patrik@centrum.cz>
2024-03-22 21:56:12 +0100

    Update metadata

--- src/components/partials/PFooter.vue
+++ src/components/partials/PFooter.vue
@@ -1,8 +1,8 @@
 <template>
   <footer class="container mx-auto min-w-full flex justify-center absolute bottom-0 h-12">
     <small
-      >Made by <a href="https://github.com/Tackx">tack</a>@<a href="https://github.com/savla-dev">savla.dev</a>. This is
-      a fan project in no way connected to Arrowhead or the official Helldivers 2 trademark.</small
+      >Made by <a href="https://github.com/Tackx">tack</a>@<a href="https://www.savla.dev">savla.dev</a>. This is a fan
+      project in no way connected to Arrowhead or the official Helldivers 2 trademark.</small
     >
   </footer>
 </template>
--- src/components/partials/PHeader.vue
+++ src/components/partials/PHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="container mx-auto min-w-full flex justify-center pt-4">
-    <h1 class="center">Helldivers 2 Loadouts</h1>
+    <h1 class="center">Helldivers 2 Loadouts Tool</h1>
   </header>
 </template>
 
