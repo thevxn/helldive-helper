e08e4ea54bca616139af4a185ccafd71babc838b
Tackx <vlk.patrik@centrum.cz>
2024-03-27 15:29:07 +0100

    Add wiki credits

--- src/components/partials/PFooter.vue
+++ src/components/partials/PFooter.vue
@@ -2,8 +2,9 @@
   <footer class="container mx-auto min-w-full flex justify-center absolute bottom-8 sm:bottom-0 h-12 p-4">
     <small
       >Made by <a href="https://github.com/Tackx">tack</a>@<a href="https://www.savla.dev">savla.dev</a>. This is a fan
-      project in no way connected to Arrowhead or the official Helldivers 2 trademark.</small
-    >
+      project in no way connected to Arrowhead or the official Helldivers 2 trademark. All imagery provided by the
+      <a href="https://helldivers.wiki.gg">Helldivers Wiki</a>.
+    </small>
   </footer>
 </template>
 
