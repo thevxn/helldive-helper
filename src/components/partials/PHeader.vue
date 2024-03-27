e252ba4ee63983c8661c5903e1bcb8d0c9e8ddd6
Tackx <vlk.patrik@centrum.cz>
2024-03-27 23:26:05 +0100

    Refactor

--- src/components/partials/PHeader.vue
+++ src/components/partials/PHeader.vue
@@ -1,9 +1,9 @@
 <template>
   <header class="container mx-auto flex min-w-full flex-col justify-center pt-4 text-center">
     <h1 class="text-center">Helldivers 2 Loadouts Tool</h1>
-    <h4 class="mt-4 p-4 text-justify sm:text-center">
+    <h2 class="mt-4 p-4 text-justify sm:text-center">
       Build up your team composition & share with your friends via the generated link!
-    </h4>
+    </h2>
   </header>
 </template>
 
