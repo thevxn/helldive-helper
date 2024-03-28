fc83f48a3dbca24e0b927a6ed6de3f0d4ef78e26
Tackx <vlk.patrik@centrum.cz>
2024-03-28 17:06:54 +0100

    Refactor types

--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -1,40 +1,37 @@
 import { reactive } from 'vue'
 
-import type { grenades } from '@/utils/grenades'
-import type { weapons } from '@/utils/weapons'
-
 export const getDefaultData = () => {
   return reactive({
     playerList: [
       {
         name: 'player1',
-        primaryWeaponCode: 'LIBERATOR' as keyof typeof weapons.primary,
-        secondaryWeaponCode: 'PEACEMAKER' as keyof typeof weapons.secondary,
-        grenadeCode: 'FRAG' as keyof typeof grenades,
+        primaryWeaponCode: 'LIBERATOR',
+        secondaryWeaponCode: 'PEACEMAKER',
+        grenadeCode: 'FRAG',
         stratagemCodeList: ['MECH', 'RAILGUN', 'JUMP_PACK', 'EAGLE_AIRSTRIKE'],
         color: 'orange'
       },
       {
         name: 'player2',
-        primaryWeaponCode: 'DILIGENCE' as keyof typeof weapons.primary,
-        secondaryWeaponCode: 'PEACEMAKER' as keyof typeof weapons.secondary,
-        grenadeCode: 'INCENDIARY' as keyof typeof grenades,
+        primaryWeaponCode: 'DILIGENCE',
+        secondaryWeaponCode: 'PEACEMAKER',
+        grenadeCode: 'INCENDIARY',
         stratagemCodeList: ['AUTOCANNON', 'RAILGUN', 'JUMP_PACK', 'EAGLE_AIRSTRIKE'],
         color: 'green'
       },
       {
         name: 'player3',
-        primaryWeaponCode: 'LIBERATOR' as keyof typeof weapons.primary,
-        secondaryWeaponCode: 'PEACEMAKER' as keyof typeof weapons.secondary,
-        grenadeCode: 'FRAG' as keyof typeof grenades,
+        primaryWeaponCode: 'LIBERATOR',
+        secondaryWeaponCode: 'PEACEMAKER',
+        grenadeCode: 'FRAG',
         stratagemCodeList: ['MECH', 'RAILGUN', 'JUMP_PACK', 'EAGLE_AIRSTRIKE'],
         color: 'blue'
       },
       {
         name: 'player4',
-        primaryWeaponCode: 'LIBERATOR' as keyof typeof weapons.primary,
-        secondaryWeaponCode: 'PEACEMAKER' as keyof typeof weapons.secondary,
-        grenadeCode: 'FRAG' as keyof typeof grenades,
+        primaryWeaponCode: 'LIBERATOR',
+        secondaryWeaponCode: 'PEACEMAKER',
+        grenadeCode: 'FRAG',
         stratagemCodeList: ['MECH', 'RAILGUN', 'JUMP_PACK', 'EAGLE_500'],
         color: 'pink'
       }
