330902394e1f5736c8582d1c7272ef5a54856e3b
Tackx <vlk.patrik@centrum.cz>
2024-03-28 16:50:24 +0100

    Rename to HH

--- src/components/partials/PHeader.vue
+++ src/components/partials/PHeader.vue
@@ -1,9 +1,7 @@
 <template>
   <header class="container mx-auto flex min-w-full flex-col justify-center pt-4 text-center">
-    <h1 class="text-center">Helldivers 2 Loadouts Tool</h1>
-    <h2 class="mt-4 p-4 text-justify sm:text-center">
-      Build up your team composition & share with your friends via the generated link!
-    </h2>
+    <h1 class="text-center">Helldive Helper</h1>
+    <h2 class="mt-4 p-4 text-justify sm:text-center">Build up your team composition & share with your friends!</h2>
   </header>
 </template>
 
