54514b07c4bc62b13b37f1aa8821a5f77a2c3936
Tackx <vlk.patrik@centrum.cz>
2024-04-17 22:06:53 +0200

    Shorten player card length, Add phone scroll snapping, Adjust stratagem selection scrolling

--- src/components/partials/PHeader.vue
+++ src/components/partials/PHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="container mx-auto flex min-w-full flex-col justify-center pt-4 text-center">
+  <header class="container mx-auto flex min-w-full snap-start flex-col justify-center pt-4 text-center">
     <h1 class="text-center">Helldive Helper</h1>
     <h2 class="mt-4 p-4 text-justify sm:text-center">Build up your team composition & share with your friends!</h2>
   </header>
--- src/views/System/HomeView.vue
+++ src/views/System/HomeView.vue
@@ -2,7 +2,7 @@
   <main
     class="container mb-16 mt-4 flex min-w-full flex-col flex-wrap gap-8 p-4 font-semibold sm:mb-4 sm:flex-row sm:gap-6 sm:p-0">
     <div
-      class="mx-auto flex h-full w-full flex-col place-items-center content-center rounded-md bg-gray-500 bg-opacity-80 p-4 sm:w-5/12 xl:w-1/5"
+      class="mx-auto flex h-full w-full snap-start flex-col place-items-center content-center rounded-md bg-gray-500 bg-opacity-80 p-4 sm:w-5/12 xl:w-1/5"
       :class="`border-2 border-solid ${playerBorders[player.color as keyof typeof playerBorders]}`"
       v-for="(player, i) in data.playerList"
       :key="i">
@@ -34,46 +34,52 @@
         :src="`/weapons/${String(player.primaryWeaponCode)}.webp`"
         class="mt-4 h-[108px] w-[250px]"
         :alt="`${(weapons.primary as typeof weapons.primary)[player.primaryWeaponCode].displayName}`" />
-      <label :for="`secondary-${i}`" class="mb-1 mt-4 w-full">Secondary Weapon:</label>
-      <select
-        name="secondary"
-        :id="`secondary-${i}`"
-        class="w-full bg-slate-700"
-        v-model="player.secondaryWeaponCode"
-        :class="`${playerInputOutlines[player.color as keyof typeof playerBorders]}`">
-        <optgroup
-          v-for="archetype in secondaryWeaponArchetypeCodeList"
-          :key="archetype"
-          :label="secondaryArchetypes[archetype].displayName">
-          <option v-for="weapon in filterArchetype(weapons.secondary, archetype)" :key="weapon" :value="weapon">
-            {{ weapons.secondary[weapon].displayName }}
-          </option>
-        </optgroup>
-      </select>
-      <img
-        :src="`/weapons/${String(player.secondaryWeaponCode)}.webp`"
-        class="mt-4 h-[108px] w-[200px]"
-        :alt="`${(weapons.secondary as typeof weapons.secondary)[player.secondaryWeaponCode].displayName}`" />
-      <label :for="`grenade-${i}`" class="mb-1 mt-4 w-full">Grenade:</label>
-      <select
-        name="grenade"
-        :id="`grenade-${i}`"
-        class="w-full bg-slate-700"
-        v-model="player.grenadeCode"
-        :class="`${playerInputOutlines[player.color as keyof typeof playerBorders]}`">
-        <optgroup
-          v-for="archetype in grenadeArchetypeCodeList"
-          :key="archetype"
-          :label="grenadeArchetypes[archetype].displayName">
-          <option v-for="grenade in filterArchetype(grenades, archetype)" :key="grenade" :value="grenade">
-            {{ grenades[grenade].displayName }}
-          </option>
-        </optgroup>
-      </select>
-      <img
-        :src="`/grenades/${String(player.grenadeCode)}.webp`"
-        class="mt-4 h-[100px] w-[100px]"
-        :alt="`${grenades[player.grenadeCode as keyof typeof grenades].displayName}`" />
+      <div class="flex h-full w-full flex-row gap-2">
+        <div class="flex w-1/2 flex-col">
+          <label :for="`secondary-${i}`" class="mb-1 mt-4 whitespace-nowrap">Secondary Weapon:</label>
+          <select
+            name="secondary"
+            :id="`secondary-${i}`"
+            class="w-[100%] bg-slate-700"
+            v-model="player.secondaryWeaponCode"
+            :class="`${playerInputOutlines[player.color as keyof typeof playerBorders]}`">
+            <optgroup
+              v-for="archetype in secondaryWeaponArchetypeCodeList"
+              :key="archetype"
+              :label="secondaryArchetypes[archetype].displayName">
+              <option v-for="weapon in filterArchetype(weapons.secondary, archetype)" :key="weapon" :value="weapon">
+                {{ weapons.secondary[weapon].displayName }}
+              </option>
+            </optgroup>
+          </select>
+          <img
+            :src="`/weapons/${String(player.secondaryWeaponCode)}.webp`"
+            class="mt-4 h-[108px] w-[200px] self-center"
+            :alt="`${(weapons.secondary as typeof weapons.secondary)[player.secondaryWeaponCode].displayName}`" />
+        </div>
+        <div class="flex w-1/2 flex-col">
+          <label :for="`grenade-${i}`" class="mb-1 mt-4">Grenade:</label>
+          <select
+            name="grenade"
+            :id="`grenade-${i}`"
+            class="w-[100%] bg-slate-700"
+            v-model="player.grenadeCode"
+            :class="`${playerInputOutlines[player.color as keyof typeof playerBorders]}`">
+            <optgroup
+              v-for="archetype in grenadeArchetypeCodeList"
+              :key="archetype"
+              :label="grenadeArchetypes[archetype].displayName">
+              <option v-for="grenade in filterArchetype(grenades, archetype)" :key="grenade" :value="grenade">
+                {{ grenades[grenade].displayName }}
+              </option>
+            </optgroup>
+          </select>
+          <img
+            :src="`/grenades/${String(player.grenadeCode)}.webp`"
+            class="mt-4 h-[100px] w-[100px] self-center"
+            :alt="`${grenades[player.grenadeCode as keyof typeof grenades].displayName}`" />
+        </div>
+      </div>
       <span class="mb-1 mt-4 w-full">Stratagems:</span>
       <div class="flex flex-row flex-wrap justify-center gap-2">
         <StratagemSelect
@@ -204,11 +210,15 @@
       modalRef.value[playerIndex].displayOn()
     }
   }
-  const handleStratagemSelection = (playerIndex: number, position: number, stratagemCode: keyof typeof stratagems) => {
-    data.playerList[playerIndex].stratagemCodeList[position] = stratagemCode
-    activeStratagemSelect.value[playerIndex][position] = false
+  const handleStratagemSelection = (
+    playerIndex: number,
+    stratagemPosition: number,
+    stratagemCode: keyof typeof stratagems
+  ) => {
+    data.playerList[playerIndex].stratagemCodeList[stratagemPosition] = stratagemCode
+    activeStratagemSelect.value[playerIndex][stratagemPosition] = false
     modalRef.value[playerIndex].displayOff()
-    document.querySelector(`#secondary-${playerIndex}`)?.scrollIntoView({ behavior: 'smooth' })
+    document.querySelector(`#primary-${playerIndex}`)?.scrollIntoView({ behavior: 'smooth' })
     modalRef.value[playerIndex].playerIndex = null
     modalRef.value[playerIndex].position = null
   }
