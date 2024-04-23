c90a0c1b9123acf47251c0f2ba191038d873622c
Tackx <vlk.patrik@centrum.cz>
2024-04-23 17:37:32 +0200

    Put logo on separate row on phone

--- src/components/partials/PHeader.vue
+++ src/components/partials/PHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="container mx-auto flex min-w-full snap-start flex-col justify-center pt-4 text-center">
-    <div class="flex flex-row justify-center gap-4">
+    <div class="flex flex-col items-center justify-center gap-4 sm:flex-row">
       <img src="/logo.png" width="50px" height="50px" />
       <h1 class="text-center font-semibold">Helldive Helper</h1>
     </div>
