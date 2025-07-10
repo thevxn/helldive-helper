a1c865d50a15b6b107a7d7f9d939b6cd6a3c1f21
Tackx <tack@vxn.dev>
2025-07-11 00:07:59 +0200

    Fix up styles, Add name labels

--- src/components/AttachmentSelect.vue
+++ src/components/AttachmentSelect.vue
@@ -3,33 +3,43 @@
     tabindex="0"
     ref="attachmentSelect"
     @keydown.esc="$emit('attachment-selected', playerIndex, position)"
-    class="bg-diagonal-solid absolute z-10 mt-4 flex h-[250px] w-[88%] -translate-y-[-52%] translate-x-[0%] snap-start flex-col overflow-y-auto rounded-md border-4 border-solid border-yellow-300 focus:outline-none sm:w-[40%] sm:-translate-y-[3%] xl:w-[21%]"
+    class="bg-diagonal-solid absolute z-10 mt-4 flex h-[350px] w-[88%] -translate-y-[-8%] translate-x-[0%] snap-start flex-col overflow-y-auto rounded-md border-4 border-solid border-yellow-300 focus:outline-none sm:w-[40%] sm:-translate-y-[31%] xl:w-[21%]"
     @click.stop
     v-if="display">
     <div class="flex h-fit w-full flex-row">
-      <div class="ml-auto mr-2 mt-2" @click="$emit('attachment-selected', playerIndex, position)">
+      <div class="ml-auto mr-3 mt-2" @click="$emit('attachment-selected', playerIndex, position)">
         <button
           class="h-8 w-8 snap-center place-self-center rounded border-2 border-solid border-yellow-300 bg-yellow-300 font-bold text-black hover:bg-black hover:text-yellow-300 active:bg-yellow-300 active:bg-none active:text-black">
           X
         </button>
       </div>
     </div>
-    <div class="flex-roow ml-auto mr-auto flex h-full w-full flex-wrap gap-2 p-3">
+    <div class="ml-auto mr-auto flex h-full w-full flex-row flex-wrap gap-2 p-3">
       <div
-        class="flex h-[80px] w-[20%] flex-row items-center justify-center rounded border-4 border-solid border-gray-900 bg-black bg-opacity-70 hover:cursor-pointer hover:border-yellow-300"
+        class="flex max-h-[50%] w-[30%] flex-col rounded border-4 border-solid border-gray-900 bg-black bg-opacity-70 last:pb-4 hover:cursor-pointer hover:border-yellow-300"
         v-for="attachment in availableAttachments"
-        :key="attachment"
-        @click="$emit('attachment-selected', playerIndex, position, attachment)">
-        <img
-          class="max-h-[64px]"
-          :src="props.primaryWeaponCode ? getAttachmentImageSource(attachment, props.primaryWeaponCode) : ''"
-          :title="
-            (
-              attachments[attachmentCategory as AttachmentCategory][
-                attachment as keyof (typeof attachments)[AttachmentCategory]
-              ] as IAttachment
-            ).displayName
-          " />
+        :key="attachment">
+        <div
+          class="flex h-[80px] flex-col items-center justify-center"
+          @click="$emit('attachment-selected', playerIndex, position, attachment)">
+          <img
+            class="max-h-[64px]"
+            :src="props.primaryWeaponCode ? getAttachmentImageSource(attachment, props.primaryWeaponCode) : ''"
+            :title="
+              (
+                attachments[attachmentCategory as AttachmentCategory][
+                  attachment as keyof (typeof attachments)[AttachmentCategory]
+                ] as IAttachment
+              ).displayName
+            " />
+        </div>
+        <span class="break-words text-center text-sm">{{
+          (
+            attachments[attachmentCategory as AttachmentCategory][
+              attachment as keyof (typeof attachments)[AttachmentCategory]
+            ] as IAttachment
+          ).displayName
+        }}</span>
       </div>
     </div>
   </div>
